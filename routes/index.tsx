e0ba15a5ac5098ea0509cce92f8f5eaebdcb8913
ruuiisu <luis.gjokaj@gmail.com>
2022-06-03 09:21:54 +0200

    3

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,20 @@
 /** @jsx h */
 /** @jsxFrag Fragment */
 
-import { h, tw, Fragment } from "../client_deps.ts";
+import { h, tw, Head, Fragment } from "../client_deps.ts";
 import Counter from "../islands/Counter.tsx";
 import SiteHead from "-/components/Head.tsx";
 
 export default function Home() {
   return (
     <>
-      <SiteHead title="Home" />
-      <div  class={tw``}>
+      <Head>
+        <title>To the Moon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        class={tw`bg-gradient-to-t from-gray-900 to-gray-200 h-screen h-screen`}
+      >
         <p>
           Welcome to `fresh`. Try update this message in the ./routes/index.tsx
           file, and refresh.
