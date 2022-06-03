11ebc69142571c59fff7d642886f26e9dbb209fe
iccee0 <rouven@12i.ch>
2022-06-03 08:49:09 +0200

    fresh init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# M346B
-Schulprojekt für M226B &amp; M120
+# fresh project
+
+### Usage
+
+Start the project:
+
+```
+deno task start
+```
+
+After adding, removing, or moving a page in the `routes` or directory, or adding,
+removing, or moving an island in the `islands` directory, run:
+
+```
+fresh manifest
+```
---
+++ routes/index.tsx
@@ -0,0 +1,21 @@
+/** @jsx h */
+import { h, tw, Head, Fragment } from "../client_deps.ts";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+      <title>To the Moon</title>
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div  class={tw`bg-gradient-to-t from-gray-900 to-gray-200 h-screen h-screen`}>
+        <p>
+          Welcome to `fresh`. Try update this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+        <Counter start={3} />
+      </div>
+    </>
+  );
+}
