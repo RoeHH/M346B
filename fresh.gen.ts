94dec2174f7b570410d456cd9a5d6b6b63f44f39
iccee0 <rouven@12i.ch>
2022-06-03 09:20:33 +0200

    hmm

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,18 +2,20 @@
 // This file SHOULD be checked into source version control.
 // To update this file, run `fresh manifest`.
 
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/_render.ts";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $0 from "./routes/_render.ts";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/timer/[name].tsx";
+import * as $3 from "./routes/timer/edit/[name].tsx";
+import * as $4 from "./routes/timer/new.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/_render.ts": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/_render.ts": $0,
+    "./routes/index.tsx": $1,
+    "./routes/timer/[name].tsx": $2,
+    "./routes/timer/edit/[name].tsx": $3,
+    "./routes/timer/new.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,15 @@
 /** @jsx h */
-import { h, tw, Head, Fragment } from "../client_deps.ts";
+/** @jsxFrag Fragment */
+
+import { h, tw, Fragment } from "../client_deps.ts";
 import Counter from "../islands/Counter.tsx";
+import SiteHead from "-/components/Head.tsx";
 
 export default function Home() {
   return (
     <>
-      <Head>
-      <title>To the Moon</title>
-      <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div  class={tw`bg-gradient-to-t from-gray-900 to-gray-200 h-screen h-screen`}>
+      <SiteHead title="Home" />
+      <div  class={tw``}>
         <p>
           Welcome to `fresh`. Try update this message in the ./routes/index.tsx
           file, and refresh.
