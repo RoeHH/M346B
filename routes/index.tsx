c1abfe4267149535845e9190c000e2b43907d7b8
iccee0 <rouven@12i.ch>
2022-06-03 10:01:10 +0200

    data

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,14 @@
 /** @jsx h */
 /** @jsxFrag Fragment */
 
-import { h, tw, Head, Fragment } from "../client_deps.ts";
-import Counter from "../islands/Counter.tsx";
+import { h, tw, Fragment } from "../client_deps.ts";
+import Counter from "-/islands/Counter.tsx";
 import SiteHead from "-/components/Head.tsx";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>To the Moon</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <SiteHead title="Home" />
       <div
         class={tw`bg-gradient-to-t from-gray-900 to-gray-200 h-screen h-screen`}
       >
