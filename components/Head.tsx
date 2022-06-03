5e0d4c35333d28bf0c233984fba4d76571477a04
ruuiisu <luis.gjokaj@gmail.com>
2022-06-03 11:25:53 +0200

    view

--- components/Head.tsx
+++ components/Head.tsx
@@ -4,14 +4,14 @@
 import { h, tw, Head, Fragment } from "-/client_deps.ts";
 
 interface SiteHeadProps {
-    title: string
+  title: string;
 }
 
 export default function SiteHead(props: SiteHeadProps) {
-    return (
-        <Head>
-            <title>{props.title}</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    )
+  return (
+    <Head>
+      <title>{props.title}</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
 }
