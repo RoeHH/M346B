3c25a93cc6da573ebae669b126853b60e9716868
ruuiisu <luis.gjokaj@gmail.com>
2022-06-10 14:03:43 +0200

    h

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,17 +6,21 @@ import SiteHead from "-/components/Head.tsx";
 import TimerCard from "-/components/TimerCard.tsx";
 
 import Timer from "-/data/interfaces/ITimer.ts";
-import { TimerDataController } from "-/data/timerDataController.ts";
+import { TimerDataController } from "-/data/TimerDataController.ts";
 
 const Timers: Timer[] = TimerDataController.getAllTimers();
 
 export default function Home() {
-    return (
+  return (
     <>
       <SiteHead title="Home" stylesheets={[]} />
-      <img src="favicon.ico" class={tw`my-10 mx-auto h-40 w-40`}/>
-      <div class={tw`mx-10 md:mx-20 lg:mx-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6`}>
-        {Timers.map(t => <TimerCard name={t.name} intervale={t.intervale} />)}
+      <img src="favicon.ico" class={tw`my-10 mx-auto h-40 w-40`} />
+      <div
+        class={tw`mx-10 md:mx-20 lg:mx-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6`}
+      >
+        {Timers.map((t) => (
+          <TimerCard name={t.name} intervale={t.intervale} />
+        ))}
       </div>
     </>
   );
--- routes/timer/edit/[name].tsx
+++ routes/timer/edit/[name].tsx
@@ -1,6 +1,56 @@
 /** @jsx h */
-import { h, PageProps } from "-/client_deps.ts";
+/** @jsxFrag Fragment */
+
+import { h, PageProps, tw, Fragment } from "-/client_deps.ts";
+import { Handlers } from "-/server_deps.ts";
+import SiteHead from "-/components/Head.tsx";
+import { TimerDataController } from "-/data/TimerDataController.ts";
+import Timer from "-/data/interfaces/ITimer.ts";
+
+interface Data {
+  queryName: string;
+  queryInterval: string;
+  success: boolean;
+}
+
+export const handler: Handlers<Data> = {
+  GET(req, ctx) {
+    const url = new URL(req.url);
+    const queryName = url.searchParams.get("name") || "";
+    const queryInterval = url.searchParams.get("interval") || "";
+    const interval = queryInterval.split(",").map(function (item) {
+      return Number(item.trim());
+    });
+    const editedTimer: Timer = {
+      name: queryName,
+      intervale: interval,
+    };
+    TimerDataController.editTimer(ctx.params.name, editedTimer);
+    return ctx.render({ queryName, queryInterval, success: true });
+  },
+};
 
 export default function Greet(props: PageProps) {
-  return <div>Welcome to the Edit page for:{props.params.name} this view will be created soon</div>;
+  return (
+    <>
+      <SiteHead title="Edit" stylesheets={["/css/main.css"]} />
+      <div className="body">
+        <h1 className={tw`text-2xl`}>Edit</h1>
+        <form className={tw`flex flex-col`}>
+          <input
+            type="text"
+            placeholder="Timer 1"
+            className={tw`h-8 w-48 p-3 m-3`}
+            name="name"
+          ></input>
+          <input
+            type="text"
+            placeholder="Timer iterationen"
+            className={tw`h-8 w-48 p-3 m-3`}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
