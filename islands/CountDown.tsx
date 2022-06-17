a0e8e3f08ee9d2bc09ef2d3f11a1150f4cb90188
ruuiisu <luis.gjokaj@gmail.com>
2022-06-17 11:23:21 +0200

    hj

--- islands/CountDown.tsx
+++ islands/CountDown.tsx
@@ -2,11 +2,13 @@
 import { h, useMemo, tw, useState, useEffect } from "../client_deps.ts";
 
 export default function CountDown({ time: intervale }: { time: number[] }) {
+  let [j, setJ] = useState(0);
   const [i, setI] = useState(1);
   const [isPaused, setIsPaused] = useState(false);
   const [seconds, setSeconds] = useState(intervale[0]);
 
   const nextInterval = () => {
+    setJ(j + 1);
     setI(i + 1);
     setSeconds(intervale[i]);
   };
@@ -31,38 +33,42 @@ export default function CountDown({ time: intervale }: { time: number[] }) {
   return (
     <div>
       <p class={tw`text-2xl`} id="countdown">
-      <svg width="400" height="400">
-        <g transform="rotate(-90 100 100)">
-          <circle
-            r="140"
-            cx="00"
-            cy="200"
-            fill="transparent"
-            stroke="lightgrey"
-            stroke-width="2rem"
-            stroke-dasharray="879.6"
-            stroke-dashoffset="0"
-          ></circle>
-          <circle
-            r="140"
-            cx="00"
-            cy="200"
-            fill="transparent"
-            stroke="black"
-            stroke-width="2rem"
-            stroke-dasharray="879.69"
-            stroke-dashoffset="132"
-          ></circle>
-        </g>
-        <text
-          x="50%"
-          y="50%"
-          dominant-baseline="central"
-          text-anchor="middle"
-        >{seconds || 0}s</text>
-      </svg>
+        <svg width="400" height="400">
+          <g transform="rotate(-90 100 100)">
+            <circle
+              r="140"
+              cx="00"
+              cy="200"
+              fill="transparent"
+              stroke="lightgrey"
+              stroke-width="2rem"
+              stroke-dasharray="879.6"
+              stroke-dashoffset="0"
+            ></circle>
+            <circle
+              r="140"
+              cx="00"
+              cy="200"
+              fill="transparent"
+              stroke="black"
+              stroke-width="2rem"
+              stroke-dasharray="879.69"
+              stroke-dashoffset={879.69 - (seconds / intervale[j]) * 879.69}
+            ></circle>
+          </g>
+          <text
+            x="50%"
+            y="50%"
+            dominant-baseline="central"
+            text-anchor="middle"
+          >
+            {seconds || 0}s
+          </text>
+        </svg>
       </p>
-      <div class={tw`fixed left-1/2 top-2/3 mt-24 w-32 2xl:left-2/3 2xl:top-1/3`}>
+      <div
+        class={tw`fixed left-1/2 top-2/3 mt-24 w-32 2xl:left-2/3 2xl:top-1/3`}
+      >
         <p class={tw`text-xl`}>Next Interval:</p>
         <p>{intervale[i] ? intervale[i] + "s" : "Done!"}</p>
       </div>
