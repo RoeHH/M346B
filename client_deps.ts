888d5a0d290d8bf531e09b4cba278dc5b7e89e99
iccee0 <rouven@12i.ch>
2022-09-21 12:28:29 +0200

    :)

--- client_deps.ts
+++ client_deps.ts
@@ -1,4 +1,4 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/dd2548a322293e6029bc298f96767a7c03bb1d7e/runtime.ts";
 import { IS_BROWSER } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
 import { apply, setup, tw } from "https://esm.sh/twind@0.16.16";
 import * as colors from "https://esm.sh/twind@0.16.16/colors";
